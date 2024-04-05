dd13e96d58ec01e5e6e3dd4046d6b44567dce7cb
Rilas2023 <said.koudil@gmail.com>
2024-04-04 22:08:31 -0500

    conflict 3 message

--- src/test/java/workflow/Login.java
+++ src/test/java/workflow/Login.java
@@ -5,5 +5,6 @@ public class Login {
         System.out.println("login page");
         System.out.println("I love you all");
         System.out.println("You can do it!");
+        System.out.println("conflict 3");
     }
 }
