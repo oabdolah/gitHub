bd084035d6806146c455dcb79448f5010bafc41d
Salma55 <salmamohamed.sa81@gmail.com>
2024-04-04 23:25:44 -0400

    conflict created by salma

--- src/test/java/workflow/Login.java
+++ src/test/java/workflow/Login.java
@@ -2,7 +2,8 @@ package workflow;
 
 public class Login {
     public static void main(String[] args) {
-        System.out.println("login page");
-        System.out.println("I love you all");
+        System.out.println("login , page");
+        System.out.println("I love you all  ");
+        System.out.println("hello world");
     }
 }
--- src/test/java/workflow/Logout.java
+++ src/test/java/workflow/Logout.java
@@ -1,4 +1,8 @@
 package workflow;
 
 public class Logout {
+
+    public static void main(String[] args) {
+        System.out.println(" I love java");
+    }
 }
