09eea127991c677b3d4ca806b9b37247749f1134
Rilas2023 <said.koudil@gmail.com>
2024-04-05 22:39:33 -0500

    messed up FeatureB, Login, Logout, SK1

--- src/test/java/workflow/Logout.java
+++ src/test/java/workflow/Logout.java
@@ -4,7 +4,9 @@ public class Logout {
 
     public static void main(String[] args) {
         System.out.println(" I love java");
-
-        System.out.println( "new changes");
+        //conflict
+        System.out.println( "new changes");//conflict
+        //conflict
+        //conflict
     }
 }
